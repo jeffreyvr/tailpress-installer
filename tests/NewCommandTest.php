c29b5d9fcb67fca734e8db1ebb574770221028ec
Alexandru Manase <malexandru699@gmail.com>
2022-11-08 22:45:03 +0200

    Create the directory for testing before every test (#6)
    * create the directory for testing before every test
    
    * cleanup

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -9,10 +9,23 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class NewCommandTest extends TestCase
 {
+    protected string $testDirectory = 'tests-output';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $testDirectoryName = __DIR__ . '/../' . $this->testDirectory;
+
+        if (!file_exists($testDirectoryName)) {
+            exec("mkdir " . __DIR__ . '/../' . $this->testDirectory);
+        }
+    }
+
     public function test_it_can_scaffold_a_new_tailpress_theme()
     {
-        $scaffoldDirectoryName = 'tests-output/just-tailpress';
-        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
+        $scaffoldDirectoryName =  $this->testDirectory . '/just-tailpress';
+        $scaffoldDirectory = __DIR__ . '/../' . $scaffoldDirectoryName;
 
         if (file_exists($scaffoldDirectory)) {
             if (PHP_OS_FAMILY == 'Windows') {
@@ -30,14 +43,14 @@ class NewCommandTest extends TestCase
         $tester->execute(['folder' => $scaffoldDirectoryName, '--name' => 'Just TailPress']);
 
         $this->assertDirectoryExists($scaffoldDirectory);
-        $this->assertFileExists($scaffoldDirectory.'/functions.php');
-        $this->assertStringContainsString('just_tailpress', file_get_contents($scaffoldDirectory.'/functions.php'));
+        $this->assertFileExists($scaffoldDirectory . '/functions.php');
+        $this->assertStringContainsString('just_tailpress', file_get_contents($scaffoldDirectory . '/functions.php'));
     }
 
     public function test_it_can_scaffold_a_new_tailpress_theme_with_wordpress()
     {
-        $scaffoldDirectoryName = 'tests-output/with-wordpress';
-        $scaffoldDirectory = __DIR__.'/../'.$scaffoldDirectoryName;
+        $scaffoldDirectoryName = $this->testDirectory . '/with-wordpress';
+        $scaffoldDirectory = __DIR__ . '/../' . $scaffoldDirectoryName;
 
         if (file_exists($scaffoldDirectory)) {
             if (PHP_OS_FAMILY == 'Windows') {
@@ -55,8 +68,10 @@ class NewCommandTest extends TestCase
         $tester->execute(['folder' => $scaffoldDirectoryName, '--name' => 'Just TailPress', '--wordpress' => true]);
 
         $this->assertDirectoryExists($scaffoldDirectory);
-        $this->assertFileExists($scaffoldDirectory.'/wp-content/themes/with-wordpress/functions.php');
-        $this->assertStringContainsString('with_wordpress',
-            file_get_contents($scaffoldDirectory.'/wp-content/themes/with-wordpress/functions.php'));
+        $this->assertFileExists($scaffoldDirectory . '/wp-content/themes/with-wordpress/functions.php');
+        $this->assertStringContainsString(
+            'with_wordpress',
+            file_get_contents($scaffoldDirectory . '/wp-content/themes/with-wordpress/functions.php')
+        );
     }
 }
